4128448c495a4c49233a6f66d35c6c7d83a599c0
hjw2044 <hjw2044@163.com>
2018-10-31 20:23:48 +0800

    20:23

--- js/interview.js
+++ js/interview.js
@@ -26,5 +26,55 @@ $(function(){
 
 
 
+    //±êÇ©À¸ÇÐ»»
+    $('.homepage').click(function(){
+        $('.video').addClass('d-none');
+        $('.unvideo').removeClass('d-none');
+        $('.mine').addClass('d-none');
+        $('.unmine').removeClass('d-none');
+    })
+    $('.unhomepage').click(function(){
+        $('.homepage').removeClass('d-none');
+        $('.unhomepage').addClass('d-none');
+        $('.video').addClass('d-none');
+        $('.unvideo').removeClass('d-none');
+        $('.mine').addClass('d-none');
+        $('.unmine').removeClass('d-none');
+    })
+    $('.unvideo').click(function(){
+        $('.video').removeClass('d-none');
+        $('.unvideo').addClass('d-none');
+        $('.homepage').addClass('d-none');
+        $('.unhomepage').removeClass('d-none');
+        $('.unmine').removeClass('d-none');
+        $('.mine').addClass('d-none');
+    })
+    $('.unmine').click(function(){
+        $('.mine').removeClass('d-none');
+        $('.unmine').addClass('d-none');
+        $('.homepage').addClass('d-none');
+        $('.unhomepage').removeClass('d-none');
+        $('.video').addClass('d-none');
+        $('.unvideo').removeClass('d-none');
+    })
+
+    $('.nav>a').click(function(){
+        $(this).css({
+            color: "#333333",
+            'border-bottom':".04rem solid #4ac6ff"
+        });
+        $(this).siblings.css({
+            color: "#777777",
+            'border-bottom':"none"
+
+        });
+
+    })
+
+
+
+
+
+
 
 });
